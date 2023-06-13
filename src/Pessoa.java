8fa324491bc74973c1444c92a09791e3036fafaf
maria Luiza <maria.luizasa06@gmail.com>
2023-06-13 08:32:00 -0300

    Balao: resultado tentaiva pesssoa

--- src/Pessoa.java
+++ src/Pessoa.java
@@ -1,2 +1,13 @@
 public class Pessoa {
+
+    // Atributos
+    String nome;
+    String sobrenome;
+    int idade;
+    String genero;
+
+    public Nome gerarNome(){
+        Nome p1 =  new Nome();
+        return p1;
+    }
 }
